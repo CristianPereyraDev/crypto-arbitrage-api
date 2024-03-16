97e9248a9952b1e5d5e8d470a5b96019228783f8
Cristian Pereyra <cristian.pereyra.dev@gmail.com>
2024-03-16 15:27:43 -0300

    Fixed problem with some dependencies

--- src/utils/apis/crypto_exchanges/p2p/trubit.ts
+++ src/utils/apis/crypto_exchanges/p2p/trubit.ts
@@ -1,32 +1,32 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import { ExchangeCollectorReturnType } from '../index.js'
+// /* eslint-disable @typescript-eslint/no-explicit-any */
+// import { ExchangeCollectorReturnType } from '../index.js'
 
-export async function getPairPrices(
-  baseAsset: string,
-  quoteAsset: string
-): Promise<ExchangeCollectorReturnType | undefined> {
-  try {
-    const apiResponse = await fetch(
-      `https://api.mexo.io/openapi/quote/v1/option/depth?symbol=${baseAsset}${quoteAsset}`
-    )
+// export async function getPairPrices(
+//   baseAsset: string,
+//   quoteAsset: string
+// ): Promise<ExchangeCollectorReturnType | undefined> {
+//   try {
+//     const apiResponse = await fetch(
+//       `https://api.mexo.io/openapi/quote/v1/option/depth?symbol=${baseAsset}${quoteAsset}`
+//     )
 
-    if (apiResponse.ok) {
-      const apiResponseJson: any = await apiResponse.json()
+//     if (apiResponse.ok) {
+//       const apiResponseJson: any = await apiResponse.json()
 
-      return {
-        asks: apiResponseJson.asks.map((ask: string[]) => [
-          parseFloat(ask[0]),
-          parseFloat(ask[1])
-        ]),
-        bids: apiResponseJson.bids.map((bid: string[]) => [
-          parseFloat(bid[0]),
-          parseFloat(bid[1])
-        ])
-      }
-    } else {
-      return undefined
-    }
-  } catch (error) {
-    console.error(error)
-  }
-}
+//       return {
+//         asks: apiResponseJson.asks.map((ask: string[]) => [
+//           parseFloat(ask[0]),
+//           parseFloat(ask[1])
+//         ]),
+//         bids: apiResponseJson.bids.map((bid: string[]) => [
+//           parseFloat(bid[0]),
+//           parseFloat(bid[1])
+//         ])
+//       }
+//     } else {
+//       return undefined
+//     }
+//   } catch (error) {
+//     console.error(error)
+//   }
+// }
