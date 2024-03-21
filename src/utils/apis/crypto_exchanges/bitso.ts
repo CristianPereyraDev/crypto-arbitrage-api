a1fc0fde1bfe42c8961becb0ef5f61fdf9f2d329
Cristian Pereyra <cristian.pereyra.dev@gmail.com>
2024-03-21 10:03:30 -0300

    Updated Bitso pricing collector.

--- src/utils/apis/crypto_exchanges/bitso.ts
+++ src/utils/apis/crypto_exchanges/bitso.ts
@@ -1,68 +1,79 @@
+import { APIError } from "src/types/errors/index.js";
 import { IExchangePairPrices } from "../../../databases/model/exchange.model.js";
 import { IPair } from "../../../databases/model/exchange_base.model.js";
-//import { fetchWithTimeout } from "../../../utils/network.utils.js";
+import { fetchWithTimeout } from "../../../utils/network.utils.js";
+
+export type BitsoAPIOrderbookResponse = {
+	success: boolean;
+	payload?: {
+		updated_at: string;
+		sequence: string;
+		bids: { book: string; price: string; amount: string }[];
+		asks: { book: string; price: string; amount: string }[];
+	};
+	error?: { message: string; code: string };
+};
 
 export async function getPairPrices(
 	pairs: IPair[],
 ): Promise<IExchangePairPrices[]> {
-	// let asks: number[][] = [];
-	// let bids: number[][] = [];
+	return await Promise.all(
+		pairs.map(
+			(pair) =>
+				new Promise<IExchangePairPrices>((resolve, reject) => {
+					const endpoint = `https://sandbox.bitso.com/api/v3/order_book/?book=${pair.crypto}_${pair.fiat}`;
 
-	// const responseBTCFiat = await fetchWithTimeout(
-	// 	"https://sandbox.bitso.com/api/v3/order_book/?book=btc_ars",
-	// );
+					fetchWithTimeout(endpoint)
+						.then((response) => {
+							if (!response.ok) {
+								reject(
+									new APIError(
+										endpoint,
+										"Bitso",
+										`${response.status}: ${response.statusText}`,
+									),
+								);
+							}
 
-	// if (responseBTCFiat.ok) {
-	// 	const jsonResponseBTCFiat: any = await responseBTCFiat.json();
+							return response.json();
+						})
+						.then((jsonResponse) => {
+							const data = jsonResponse as BitsoAPIOrderbookResponse;
 
-	// 	switch (baseAsset) {
-	// 		case "BTC":
-	// 			asks = jsonResponseBTCFiat.payload.asks.map((ask: any) => [
-	// 				parseFloat(ask.price),
-	// 				parseFloat(ask.amount),
-	// 			]);
-	// 			// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	// 			bids = jsonResponseBTCFiat.payload.bids.map((bid: any) => [
-	// 				parseFloat(bid.price),
-	// 				parseFloat(bid.amount),
-	// 			]);
-	// 			break;
-	// 		default:
-	// 			// eslint-disable-next-line no-case-declarations
-	// 			const responseAssetFiat = await fetch(
-	// 				`https://sandbox.bitso.com/api/v3/ticker/?book=btc_${baseAsset.toLowerCase()}`,
-	// 			);
-
-	// 			if (responseAssetFiat.ok) {
-	// 				// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	// 				const jsonResponseBTCAsset: any = await responseAssetFiat.json();
-	// 				const priceBTCAsset = parseFloat(jsonResponseBTCAsset.payload.bid);
-
-	// 				// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	// 				asks = jsonResponseBTCFiat.payload.asks.map((ask: any) => [
-	// 					parseFloat(ask.price) / priceBTCAsset,
-	// 					parseFloat(ask.amount),
-	// 				]);
-	// 				// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	// 				bids = jsonResponseBTCFiat.payload.bids.map((bid: any) => [
-	// 					parseFloat(bid.price) / priceBTCAsset,
-	// 					parseFloat(bid.amount),
-	// 				]);
-	// 			} else {
-	// 				return undefined;
-	// 			}
-	// 	}
-
-	// 	return {
-	// 		asks,
-	// 		bids,
-	// 	};
-	// }
-	return pairs.map((pair) => {
-		return {
-			crypto: pair.crypto,
-			fiat: pair.fiat,
-			asksAndBids: { asks: [], bids: [] },
-		};
-	});
+							if (data.success && data.payload) {
+								resolve({
+									crypto: pair.crypto,
+									fiat: pair.fiat,
+									asksAndBids: {
+										asks: data.payload.asks.map((ask) => [
+											parseFloat(ask.price),
+											parseFloat(ask.amount),
+										]),
+										bids: data.payload.bids.map((bid) => [
+											parseFloat(bid.price),
+											parseFloat(bid.amount),
+										]),
+									},
+								});
+							} else {
+								reject(
+									new APIError(
+										endpoint,
+										"Bitso",
+										data.error?.message ??
+											`There was an error when fetching the ${pair.crypto}${pair.fiat} prices`,
+									),
+								);
+							}
+						})
+						.catch((reason) =>
+							reject(
+								new Error(
+									`There was an error when fetching the ${pair.crypto}${pair.fiat} prices from Bitso :${reason}`,
+								),
+							),
+						);
+				}),
+		),
+	);
 }
